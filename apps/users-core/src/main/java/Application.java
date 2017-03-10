2fd3253ee00ab1b766916aa2714c44e745dff8d5
Mariusz Sowa <mariuszsowa11@gmail.com>
2017-03-10 12:32:38 +0100

    eureka init

--- apps/users-core/src/main/java/Application.java
+++ apps/users-core/src/main/java/Application.java
@@ -1,27 +1,18 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
-import sample.project.model.User;
 
 /**
  * Created by sovas on 05.03.2017.
  */
 @SpringBootApplication
 @ComponentScan(basePackages = "sample.project")
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
-
         SpringApplication.run(Application.class, args);
-
-        User user = User.builder()
-                .id(1)
-                .firstName("Marian")
-                .lastName("Paździoch")
-                .build();
-
-
-        System.out.println("\n" + user.toString() + "\n");
     }
 
 }
